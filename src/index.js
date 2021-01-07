bf7b4b8fea2fc2487d3e76ea512a8d9560266f34
jameil <kingkojah@Jameils-Mac-mini.local>
2021-01-06 16:58:26 -0800

    my first commit using git

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router } from 'react-router-dom';
+
+import Routes from 'routes';
+import Topbar from 'components/topbar';
+import {CurrentUserProvider} from 'contexts/currentUser';
+import CurrentUserChecker from 'components/currentUserChecker';
+
+const App = () => {
+  return(
+  <CurrentUserProvider>
+    <CurrentUserChecker>
+      <Router>
+        <Topbar /> 
+        <Routes />
+      </Router>
+    </CurrentUserChecker>
+  </CurrentUserProvider>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +26,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
